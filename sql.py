d809bb8f721a0f4c54c900eea9fd53632166da43
anfieldeire <gareth.hasson@gmail.com>
2024-10-19 12:56:28 -0400

    Add files via upload

--- sql.py
+++ sql.py
@@ -1,6 +1,9 @@
 
 
-CREATE_STUDENT = "INSERT into student (name, home_address, dob) VALUES (?,?,?)"
+CREATE_STUDENT = "INSERT into STUDENT (name, home_address, dob) VALUES (?,?,?)"
 
-GET_STUDENT = "SELECT * from student WHERE student_id = (?);"
+GET_STUDENT = "SELECT * from STUDENT WHERE student_id = (?);"
 
+UPDATE_STUDENT = "UPDATE STUDENT set name=(?), home_address=(?) WHERE student_id = (?)"
+
+CREATE_COURSE = "INSERT into COURSES (name, course_id) VALUES (?, ?)"
