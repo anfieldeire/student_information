2bd5a47e6012bba2ffb84cc63be76760365daff2
anfieldeire <gareth.hasson@gmail.com>
2024-10-21 09:57:26 -0400

    Add files via upload

--- sql.py
+++ sql.py
@@ -6,4 +6,10 @@ GET_STUDENT = "SELECT * from STUDENT WHERE student_id = (?);"
 
 UPDATE_STUDENT = "UPDATE STUDENT set name=(?), home_address=(?) WHERE student_id = (?)"
 
-CREATE_COURSE = "INSERT into COURSES (name, course_id) VALUES (?, ?)"
+CREATE_COURSE = "INSERT into COURSES (name) VALUES (?)"
+
+UPDATE_COURSE = "UPDATE COURSES set name=(?) WHERE course_id=(?)"
+
+CREATE_MARK = "INSERT into MARKS (course_id, course_name, student_id, student_name, mark, year) VALUES (?,?,?,?,?,?)"
+
+GET_MARKS = "SELECT course_name, mark from MARKS WHERE course_id = (?) AND student_id = (?)"
