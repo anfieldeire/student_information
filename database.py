36bef0c928f34926992baa095a52f4428b40cfa4
anfieldeire <gareth.hasson@gmail.com>
2024-10-21 20:57:39 -0400

    Add files via upload

--- database.py
+++ database.py
@@ -1,43 +1,54 @@
 import sqlite3
 
 
-conn = sqlite3.connect('student.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+conn = sqlite3.connect('student.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES )
 curs = conn.cursor()
+
 
 # https://www.geeksforgeeks.org/python-sqlite-working-with-date-and-datetime/
 
 def create_tables():
 
-    with conn:
-        curs.execute(""" CREATE TABLE IF NOT EXISTS student (
-        name text,
-        student_id integer PRIMARY KEY,
-        home_address text,
-        dob text)""")
+    # with conn:
+    #     curs.execute(""" CREATE TABLE IF NOT EXISTS student (
+    #     name text,
+    #     student_id integer PRIMARY KEY,
+    #     home_address text,
+    #     dob text)""")
+    #
+    # with conn:
+    #     curs.execute(""" CREATE TABLE IF NOT EXISTS courses (
+    #     name text,
+    #     course_id integer PRIMARY KEY)""")
 
     with conn:
-        curs.execute(""" CREATE TABLE IF NOT EXISTS courses (
-        name text,
-        course_id integer PRIMARY KEY)""")
+        # curs.execute(""" CREATE TABLE IF NOT EXISTS marks (
 
-    with conn:
         curs.execute(""" CREATE TABLE IF NOT EXISTS marks (
         course_id integer,
+        course_name text,
         student_id integer,
-        mark integer)""")
-
-    with conn:
-        curs.execute(""" CREATE TABLE IF NOT EXISTS grades (
-        student_id integer,
-        grade text,
-        mark integer)""")
-
-    with conn:
-        curs.execute(""" CREATE TABLE IF NOT EXISTS employees (
-        employee_id integer,
-        name text,
-        faculty text,
-        dob TIMESTAMP)""")
+        student_name text,
+        mark integer,
+        year text,
+        foreign key (student_id) references student (student_id),
+        foreign key (course_id) references courses (course_id),
+        primary key (student_id, course_id))
+        """)
+        # print(var)
+    #
+    # with conn:
+    #     curs.execute(""" CREATE TABLE IF NOT EXISTS grades (
+    #     student_id integer,
+    #     grade text,
+    #     mark integer)""")
+    #
+    # with conn:
+    #     curs.execute(""" CREATE TABLE IF NOT EXISTS employees (
+    #     employee_id integer,
+    #     name text,
+    #     faculty text,
+    #     dob TIMESTAMP)""")
 
 
 if __name__ == '__main__':
