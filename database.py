36829283b0d1a22916c3745472874381fdaf6e6d
anfieldeire <gareth.hasson@gmail.com>
2024-10-24 11:09:06 -0400

    Add files via upload

--- database.py
+++ database.py
@@ -21,22 +21,20 @@ def create_tables():
     #     name text,
     #     course_id integer PRIMARY KEY)""")
 
-    with conn:
-        # curs.execute(""" CREATE TABLE IF NOT EXISTS marks (
-
-        curs.execute(""" CREATE TABLE IF NOT EXISTS marks (
-        course_id integer,
-        course_name text,
-        student_id integer,
-        student_name text,
-        mark integer,
-        year text,
-        foreign key (student_id) references student (student_id),
-        foreign key (course_id) references courses (course_id),
-        primary key (student_id, course_id))
-        """)
-        # print(var)
+    # with conn:
     #
+    #     curs.execute(""" CREATE TABLE IF NOT EXISTS marks (
+    #     course_id integer,
+    #     course_name text,
+    #     student_id integer,
+    #     student_name text,
+    #     mark integer,
+    #     year text,
+    #     foreign key (student_id) references student (student_id),
+    #     foreign key (course_id) references courses (course_id),
+    #     primary key (student_id, course_id))
+    #     """)
+
     # with conn:
     #     curs.execute(""" CREATE TABLE IF NOT EXISTS grades (
     #     student_id integer,
@@ -49,6 +47,19 @@ def create_tables():
     #     name text,
     #     faculty text,
     #     dob TIMESTAMP)""")
+
+    with conn:
+        curs.execute(""" CREATE TABLE IF NOT EXISTS degree (
+           degree_id integer,
+           degree_name text,
+           student_id integer,
+           student_name text,
+           overall_mark integer,
+           graduation_year text,
+           overall_grade,
+           foreign key (student_id) references student (student_id),
+           UNIQUE  (degree_id, student_id));
+           """)
 
 
 if __name__ == '__main__':
--- degree.py
+++ degree.py
@@ -6,7 +6,7 @@ from student import *
 
 class Degree:
 
-    def __init__(self, degree_name, student_id, overall_mark, graduation_year, overall_grade):
+    def __init__(self, degree_name, student_id, overall_mark, graduation_year):
         self.conn = sqlite3.connect('student.db')
         self.curs = self.conn.cursor()
 
@@ -14,26 +14,44 @@ class Degree:
         self.student_id = student_id
         self.overall_mark = overall_mark
         self.graduation_year = graduation_year
-        self.overall_grade = overall_grade
 
-    def create_degree(self, degree_name, student_id, overall_mark, graduation_year, overall_grade):
+
+    def set_grade(self, overall_mark):
+        if self.overall_mark >= 70:
+            self.overall_grade = "A"
+        elif self.overall_mark >= 60 & self.overall_mark < 70:
+            self.overall_grade = "B"
+            print("overall grade1")
+            print(self.overall_grade)
+        elif self.overall_mark >= 50 & self.overall_mark < 60:
+            self.overall_grade = "C"
+        elif self.overall_mark >= 40 & self.overall_mark < 50:
+            self.overall_grade = "D"
+
+        return self.overall_grade
+
+
+    def create_degree(self, degree_name, student_id, overall_mark, graduation_year):
         """ Create a degree table with degree name and student information """
 
         student_name = get_student(student_id)
-        print("student name")
-        print(student_name)
+        overall_grade = self.set_grade(overall_mark)
 
+        print("in create degree")
+        print(overall_grade)
         self.curs.execute(CREATE_DEGREE, (degree_name, student_id, student_name, overall_mark, graduation_year,
                                           overall_grade))
-        var = (degree_name, student_id, student_name, overall_mark, graduation_year, overall_grade)
-        print("var")
-        print(var)
+
+        print("Degree has been created")
         self.conn.commit()
+
+
+
 
 if __name__ == "__main__":
 
-    d1 = Degree("Bsc Hons Computing", 2,  70, "1995", "A")
-    d1.create_degree("Bsc Hons Computing", 2, 70, "1995", "A")
+    d1 = Degree("Bsc Business & Computing", 2,  60, "1992")
+    d1.create_degree("Bsc Business & Computing", 2, 60, "1992")
 
 
 
